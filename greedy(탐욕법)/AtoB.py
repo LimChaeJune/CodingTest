e2d40c4cfd9875de0ba4a5db445a9cc7689f79e4
DESKTOP-GA0G4OG\cowns <cownsgg@outlook.com>
2021-12-28 22:35:13 +0900

    A->T 문제

---
+++ greedy(탐욕법)/AtoB.py
@@ -0,0 +1,25 @@
+A,B = map(int,input().split())
+
+result = 1
+
+while(True):
+    if B == A:        
+        break
+    elif (B % 2 != 0 and B % 10 != 1) or (B < A):        
+        result = -1
+        break
+    else:
+        if B % 10 == 1:
+            B = B // 10
+            result = result + 1
+        elif B % 2 == 0 :
+            B = B // 2
+            result = result + 1
+
+
+print(result)
+    
+    
+        
+    
+
