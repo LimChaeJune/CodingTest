bac425379494b2f602190ce89b1b0a33617fe740
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-01-24 23:09:20 +0900

    주사위 굴리기 - 2시간 성장중;;

--- 삼성기출/주사위굴리기.py
+++ 삼성기출/주사위굴리기.py
@@ -1,8 +1,7 @@
-import graphlib
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+N, M, X, Y, mCnt = map(int, input().split())
 
 graph = [[0 for _ in range(M)] for _ in range(N)]
 
@@ -13,9 +12,52 @@ for i in range(N):
 
 moving = list(map(int, input().split()))        
 
-dice = []
+# 위, 아래, 오, 왼, 앞, 뒤
+dice = [0,0,0,0,0,0]
 
-for i in moving:
+dy = [0,1,-1,0,0]
+dx = [0,0,0,-1,1]
 
+
+cy = Y
+cx = X
+for i in moving:               
+    if cx + dx[i]  < 0 or cx + dx[i] >= N or cy + dy[i] < 0 or cy + dy[i] >= M:
+        continue    
+
+    cy += dy[i]
+    cx += dx[i]
+
+    copyDice = dice[:] 
+
+    if i == 1:
+        dice[0] = copyDice[3]
+        dice[1] = copyDice[2]
+        dice[2] = copyDice[0]
+        dice[3] = copyDice[1]
+    elif i == 2:
+        dice[0] = copyDice[2]
+        dice[1] = copyDice[3]
+        dice[2] = copyDice[1]
+        dice[3] = copyDice[0]
+    elif i == 3:
+        dice[0] = copyDice[4]
+        dice[1] = copyDice[5]
+        dice[4] = copyDice[1]
+        dice[5] = copyDice[0]
+    elif i == 4:
+        dice[0] = copyDice[5]
+        dice[1] = copyDice[4]
+        dice[4] = copyDice[0]
+        dice[5] = copyDice[1]
+
+    if graph[cx][cy] == 0:
+        graph[cx][cy] = dice[1]
+    else:
+        dice[1] = graph[cx][cy]
+        graph[cx][cy] = 0
+
+    print(dice[0])
+    
 
 
