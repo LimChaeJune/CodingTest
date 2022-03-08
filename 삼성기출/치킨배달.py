12915417f86e814f2898a97e6ccf1635c400f126
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-03-08 16:10:09 +0900

    치킨/ 톱니

--- 삼성기출/치킨배달.py
+++ 삼성기출/치킨배달.py
@@ -1,44 +1,26 @@
+from itertools import combinations
+N, M = map(int,input().split())
 
-import bisect
 
-
-N,M = map(int, input().split())
-
-graph = []
 stores = []
 homes = []
 for i in range(N):
-  row = list(map(int, input().split()))
-  for j in range(N):
-    graph.append(row[j])
-    if row[j] == 2: 
-      stores.append([i,j])
-    elif row[j] == 1:
-      homes.append([i,j])
+  row =  list(map(int, input().split()))
+  for j in range(len(row)):
+    if row[j] == 1:
+      homes.append((i,j))
+    elif row[j] == 2:
+      stores.append((i,j))
 
-storesVal = [0] * len(stores)
-homeVis = [99999] * len(homes)
-for i in range(len(stores)):  
-  for j in range(len(homes)):
-    x = (homes[j][0]+1) - (stores[i][0]+1)
-    y = (homes[j][1]+1) - (stores[i][1]+1)
-    chicRange =  (abs(x) + abs(y))
-    homeVis[j] = min(homeVis[j], chicRange)
-    if chicRange == homeVis[j]:
-        storesVal[i] += chicRange
-
-while len(stores) > M:
-    maxVal = max(storesVal)
-    idx = storesVal.index(maxVal)
-    storesVal.pop(idx)
-    stores.pop(idx)
-
-homeVis = [99999] * len(homes)
-for i in range(len(stores)):  
-  for j in range(len(homes)):
-    x = (homes[j][0]+1) - (stores[i][0]+1)
-    y = (homes[j][1]+1) - (stores[i][1]+1)
-    chicRange =  (abs(x) + abs(y))
-    homeVis[j] = min(homeVis[j], chicRange)    
-
-print(sum(homeVis))
+storeM = combinations(stores, M)
+answer = 99999
+for combiStore in storeM:
+  homeVis = [99999] * len(homes)
+  for combi in combiStore:
+    for j in range(len(homes)):
+      chicRan = abs(combi[0]-homes[j][0])+abs(combi[1]-homes[j][1])
+      homeVis[j] = min(homeVis[j], chicRan)
+      
+  answer = min(sum(homeVis), answer)
+  
+print(answer)
