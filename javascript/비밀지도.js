756eb0d201b74b4d0663cc0e2a013f267b6cbbbd
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2022-07-28 01:35:58 +0900

    Update 비밀지도.js

--- javascript/비밀지도.js
+++ javascript/비밀지도.js
@@ -1,13 +1,28 @@
 function solution(n, arr1, arr2) {
     var answer = [];
-    let newboard = []
-    for (let i = 0; i < n; i++){
-        let hec = arr1[i].toString(2).split("");
-        for (let j = 0; j < hec.length; j++){
-            console.log(hec[j]);
-        }
-    }
+    let newboard = Array.from(Array(n), () => Array(n).fill(0))
     
+    for (let i = 0; i < n; i++){
+        let arr1_item = arr1[i].toString(2).split("").reverse();
+        let arr2_item = arr2[i].toString(2).split("").reverse();
+        
+        for (let j = 0; j < arr1_item.length; j++){
+            if (arr1_item[j] === "1"){
+                newboard[i][j] = 1
+            }
+        }        
+        for (let k = 0; k < arr2_item.length; k++){
+            if (arr2_item[k] === "1"){
+                newboard[i][k] = 1
+            }
+        }        
+    }
+
+    for (let i = 0; i < n; i++){
+        var taew = newboard[i].reverse().map(e => e === 1 ? "#" : " ").join("");
+        answer.push(taew);
+    }
+    console.log(answer);
     return answer;
 }
 
